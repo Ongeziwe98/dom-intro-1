ebe434c3409271c473bf0308e626a5ee8ced409b
ongeziwe98 <ongeziwebaba@gmail.com>
2019-06-03 17:55:59 +0200

    made changes

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -23,23 +23,23 @@
 
 var billItemTypeWithSettings = document.querySelector(".billItemTypeWithSettings");
 var radioSettingsAddBtn = document.querySelector(".radioSettingsAddBtn");
-var callTotalSettings=  document.querySelector(".callTotalSettings");
-var smsTotalSettings =  document.querySelector(".smsTotalSettings");
-var totalSettings =  document.querySelector(".totalSettings");
+var callTotalSettings = document.querySelector(".callTotalSettings");
+var smsTotalSettings = document.querySelector(".smsTotalSettings");
+var totalSettings = document.querySelector(".totalSettings");
 //var totalCostElem = document.querySelector(".addToBillBtn");
 var updateSettings = document.querySelector(".updateSettings");
-var callCostSetting=  document.querySelector(".callCostSetting");
-var smsCostSetting =  document.querySelector(".smsCostSetting");
-var warningLevelSetting =  document.querySelector(".warningLevelSetting");
+var callCostSetting = document.querySelector(".callCostSetting");
+var smsCostSetting = document.querySelector(".smsCostSetting");
+var warningLevelSetting = document.querySelector(".warningLevelSetting");
 var criticalLevelSetting = document.querySelector(".criticalLevelSetting")
 
 var callTotal = 0;
 var smsTotal = 0;
 var totals = 0;
-var callCost=0;
-var smsCost=0;
-var warningLevel =0;
-var criticalLevel=0;
+var callCost = 0;
+var smsCost = 0;
+var warningLevel = 0;
+var criticalLevel = 0;
 
 function radioBillTotal() {
     var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
@@ -49,67 +49,82 @@ function radioBillTotal() {
             callTotal += callCost;
         }
         else if (billItemTypeWithSettings === "sms") {
-            smsTotal  += smsCost;
+            smsTotal += smsCost;
         }
-        callTotalSettings.innerHTML =  callTotal.toFixed(2);
+        callTotalSettings.innerHTML = callTotal.toFixed(2);
         smsTotalSettings.innerHTML = smsTotal.toFixed(2);
-         totals =  callTotal + smsTotal;
-         totalSettings.innerHTML = totals.toFixed(2);
+        totals = callTotal + smsTotal;
+        totalSettings.innerHTML = totals.toFixed(2);
+
+
 
         if (totals >= criticalLevel) {
             totalSettings.classList.add("danger");
         }
-        else if (totals >= warningLevel) {
-            totalSettings.classList.add("warning");
-        }
-        if (totals >= criticalLevel) {
-                radioSettingsAddBtn.disabled = true;
-      }
 
+        else if (totals >= warningLevel) {
+            totalSettings.classList.remove("warning");
+            totalSettings.classList.add("warning");
+        } 
+
+        if (totals >= criticalLevel) {
+            radioSettingsAddBtn.disabled = true;
+        }
+        if (totalSettings < warningLevel) {
+
+            totalSettings.classList.remove("warning")
+        }
     }
+
 }
-radioSettingsAddBtn.addEventListener('click', radioBillTotal);
+
 
 function Settings() {
 
-    callCost= Number(callCostSetting.value);
-    smsCost= Number(smsCostSetting .value);
-    warningLevel= warningLevelSetting.value;
-    criticalLevel= criticalLevelSetting.value;
+    callCost = Number(callCostSetting.value);
+    smsCost = Number(smsCostSetting.value);
+    warningLevel = warningLevelSetting.value;
+    criticalLevel = criticalLevelSetting.value;
 
 
-    callCostSetting.innerHTML =  callTotal.toFixed(2);
+
+    callCostSetting.innerHTML = callTotal.toFixed(2);
     smsCostSetting.innerHTML = smsTotal.toFixed(2);
-     totals =  callTotal + smsTotal;
-     totalSettings.innerHTML = totals.toFixed(2);
-     
-     if (totals < criticalLevel) {
+    totals = callTotal + smsTotal;
+    totalSettings.innerHTML = totals.toFixed(2);
+
+    if (totals < criticalLevel) {
         radioSettingsAddBtn.disabled = false;
-}
-if (totals >= criticalLevel) {
-    radioSettingsAddBtn.disabled = true;
-}
+    }
+    if (totals >= criticalLevel) {
+        radioSettingsAddBtn.disabled = true;
+    }
 
 
-if (totals >= criticalLevel) {
-    totalSettings.classList.add("danger");
-    totalSettings.classList.remove("warning");
-     
-}
-else if (totals >= warningLevel) {
-    totalSettings.classList.add("warning");
-    totalSettings.classList.remove("danger");
-}
-if (totalSettings < warningLevel){
-    totalSettings.classList.remove("warning")
-    totalSettings.classList.remove("danger")
-}
-if(criticalLevel < totalSettings){
-    totalSettings.classList.remove("warning")
-    totalSettings.classList.remove("danger")
-}
+    if (totals >= criticalLevel) {
+        totalSettings.classList.add("danger");
+        totalSettings.classList.remove("warning");
+
+    }
+    if (totals >= warningLevel) {
+        totalSettings.classList.add("warning");
+        totalSettings.classList.remove("danger");
+    }
+    if (totalSettings < warningLevel) {
+        totalSettings.classList.remove("danger")
+        totalSettings.classList.remove("warning")
+    }
+    if (criticalLevel < totalSettings) {
+       // totalSettings.classList.add("warning")
+        totalSettings.classList.remove("danger")
+    }
+
+    else {
+        totalSettings.classList.remove("warning")
+        totalSettings.classList.remove("danger")
+    }
 
 }
-updateSettings.addEventListener('click', Settings );
 
-
+radioSettingsAddBtn.addEventListener('click', radioBillTotal);
+updateSettings.addEventListener('click', Settings);
